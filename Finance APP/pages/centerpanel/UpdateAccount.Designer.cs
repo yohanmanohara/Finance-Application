417d33ebfa45f5117ac8043496f4da6323fe1ca1
Hikarunathilake <hikarunathilake@students.nsbm.ac.lk>
2024-03-31 22:32:29 +0530

    #

--- Finance APP/pages/centerpanel/UpdateAccount.Designer.cs
+++ Finance APP/pages/centerpanel/UpdateAccount.Designer.cs
@@ -87,6 +87,7 @@
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(406, 254);
             this.panel2.TabIndex = 40;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
             // 
             // txtAddress
             // 
