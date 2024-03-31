562068aae9b4debd429a4886209dfd0b206b40c5
Yashira-De-Silva <yashiradesilva@gmail.com>
2024-03-31 23:24:41 +0530

    #

---
+++ Finance APP/pages/centerpanel/transfer.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace Finance_APP.pages.centerpanel
+{
+    partial class transfer
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // transfer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "transfer";
+            this.Size = new System.Drawing.Size(904, 711);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
