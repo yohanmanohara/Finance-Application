81cd14116a1308328e370a5bd9c7c54fe92e7272
Yashira-De-Silva <yashiradesilva@gmail.com>
2024-04-01 02:38:58 +0530

    Done Success Alert

---
+++ Finance APP/pages/centerpanel/success alert.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Finance_APP.pages.centerpanel
+{
+    public partial class success_alert : UserControl
+    {
+        public success_alert()
+        {
+            InitializeComponent();
+        }
+
+        private void guna2CirclePictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
