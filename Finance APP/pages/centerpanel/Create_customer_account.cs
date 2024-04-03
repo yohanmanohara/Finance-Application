71da62aa7b196df7e219db49a11867f255d44785
yohan manohara <warushayohan80@gmail.com>
2024-04-03 12:00:28 +0530

    #

---
+++ Finance APP/pages/centerpanel/Create_customer_account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Finance_APP.pages.centerpanel
+{
+    public partial class Create_customer_account : UserControl
+    {
+        public bool TopLevel { get; internal set; }
+
+
+        public Create_customer_account()
+             
+        {
+            InitializeComponent();
+        }
+
+        private void Create_customer_account_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
