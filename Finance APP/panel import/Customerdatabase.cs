d62301d8c6f5f41a6eb508082925a26c91a67d40
Hikarunathilake <hikarunathilake@students.nsbm.ac.lk>
2024-04-07 23:05:38 +0530

    developed customer database page

--- Finance APP/panel import/Customerdatabase.cs
+++ Finance APP/panel import/Customerdatabase.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,11 +17,48 @@ namespace Finance_APP.panel_import
         public Customerdatabase()
         {
             InitializeComponent();
+            DisplayData();
         }
 
         private void Customerdatabase_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void guna2DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        SqlConnection connectionString = new SqlConnection(@"Data Source=financensbm.database.windows.net;Initial Catalog=finance;Persist Security Info=True;User ID=financeadmin;Password=finance2024$;Encrypt=True");
+
+        private void DisplayData()
+        {
+            try
+            {
+                // Create a connection to the database
+
+                {
+                    // Open the connection
+                    connectionString.Open();
+
+                    // SQL query to select data from a table
+                    string query = "SELECT * FROM customer";
+
+                    // Create a data adapter and a data table
+                    SqlDataAdapter adapter = new SqlDataAdapter(query, connectionString);
+                    DataTable dataTable = new DataTable();
+
+                    // Fill the data table with the data from the adapter
+                    adapter.Fill(dataTable);
+
+                    // Bind the data table to the DataGridView
+                    guna2DataGridView1.DataSource = dataTable;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
         }
     }
 }
