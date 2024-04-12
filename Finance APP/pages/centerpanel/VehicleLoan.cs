2d1265b7a04ead1bd3f0c2d4146a30f052c0ad8b
Vismitha Gunasekara <Vismitha65mya@gmail.com>
2024-04-12 08:19:39 +0530

    vehicleloan1

---
+++ Finance APP/pages/centerpanel/VehicleLoan.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Finance_APP.pages.centerpanel
+{
+    public partial class VehicleLoan : UserControl
+    {
+        public VehicleLoan()
+        {
+            InitializeComponent();
+        }
+
+        private void guna2CheckBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2TextBox20_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2TextBox27_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2TextBox31_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2TextBox24_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label19_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2TextBox30_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2Panel20_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
