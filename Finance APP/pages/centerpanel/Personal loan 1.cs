115f2d4c9d4f68b13e390cfa0cc1ca4c7ffa94d2
DulenW <dulenweerasinghe37@gmail.com>
2024-04-18 22:09:16 +0530

    Personal loan 1 win form done.

---
+++ Finance APP/pages/centerpanel/Personal loan 1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Finance_APP.pages.centerpanel
+{
+    public partial class Personal_loan_1 : UserControl
+    {
+        public Personal_loan_1()
+        {
+            InitializeComponent();
+        }
+    }
+}
