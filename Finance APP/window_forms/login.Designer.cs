5a0685b40bf9d1d87130d5304e04e0391564a2b9
Yasiru Dharmathilaka <yasiru.dharmathilaka@gmail.com>
2024-04-22 23:16:23 +0530

    Alignment changes

--- Finance APP/window_forms/login.Designer.cs
+++ Finance APP/window_forms/login.Designer.cs
@@ -52,7 +52,7 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.SystemColors.AppWorkspace;
-            this.label5.Location = new System.Drawing.Point(223, 425);
+            this.label5.Location = new System.Drawing.Point(256, 425);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(98, 16);
             this.label5.TabIndex = 40;
@@ -60,7 +60,7 @@
             // 
             // guna2Button1
             // 
-            this.guna2Button1.BorderRadius = 20;
+            this.guna2Button1.BorderRadius = 10;
             this.guna2Button1.CheckedState.Parent = this.guna2Button1;
             this.guna2Button1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.guna2Button1.CustomImages.Parent = this.guna2Button1;
@@ -68,7 +68,7 @@
             this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2Button1.ForeColor = System.Drawing.Color.White;
             this.guna2Button1.HoverState.Parent = this.guna2Button1;
-            this.guna2Button1.Location = new System.Drawing.Point(140, 455);
+            this.guna2Button1.Location = new System.Drawing.Point(140, 475);
             this.guna2Button1.Name = "guna2Button1";
             this.guna2Button1.ShadowDecoration.Parent = this.guna2Button1;
             this.guna2Button1.Size = new System.Drawing.Size(125, 48);
@@ -94,7 +94,7 @@
             this.u_password.ForeColor = System.Drawing.SystemColors.GrayText;
             this.u_password.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.u_password.HoverState.Parent = this.u_password;
-            this.u_password.Location = new System.Drawing.Point(63, 368);
+            this.u_password.Location = new System.Drawing.Point(39, 368);
             this.u_password.Margin = new System.Windows.Forms.Padding(4);
             this.u_password.Name = "u_password";
             this.u_password.PasswordChar = '\0';
@@ -102,7 +102,7 @@
             this.u_password.PlaceholderText = "Enter the password";
             this.u_password.SelectedText = "";
             this.u_password.ShadowDecoration.Parent = this.u_password;
-            this.u_password.Size = new System.Drawing.Size(279, 53);
+            this.u_password.Size = new System.Drawing.Size(327, 53);
             this.u_password.TabIndex = 37;
             this.u_password.UseSystemPasswordChar = true;
             // 
@@ -124,7 +124,7 @@
             this.u_name.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
             this.u_name.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.u_name.HoverState.Parent = this.u_name;
-            this.u_name.Location = new System.Drawing.Point(63, 267);
+            this.u_name.Location = new System.Drawing.Point(39, 267);
             this.u_name.Margin = new System.Windows.Forms.Padding(4);
             this.u_name.Name = "u_name";
             this.u_name.PasswordChar = '\0';
@@ -132,7 +132,7 @@
             this.u_name.PlaceholderText = "Username";
             this.u_name.SelectedText = "";
             this.u_name.ShadowDecoration.Parent = this.u_name;
-            this.u_name.Size = new System.Drawing.Size(279, 53);
+            this.u_name.Size = new System.Drawing.Size(327, 53);
             this.u_name.TabIndex = 36;
             this.u_name.TextChanged += new System.EventHandler(this.u_name_TextChanged);
             // 
@@ -166,7 +166,7 @@
             this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.pictureBox4.Image = global::Finance_APP.Properties.Resources.Lock_Icon_removebg_preview;
-            this.pictureBox4.Location = new System.Drawing.Point(347, 383);
+            this.pictureBox4.Location = new System.Drawing.Point(324, 383);
             this.pictureBox4.Name = "pictureBox4";
             this.pictureBox4.Size = new System.Drawing.Size(30, 26);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -201,7 +201,7 @@
             // 
             this.pictureBox5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
-            this.pictureBox5.Location = new System.Drawing.Point(140, 111);
+            this.pictureBox5.Location = new System.Drawing.Point(140, 104);
             this.pictureBox5.Name = "pictureBox5";
             this.pictureBox5.Size = new System.Drawing.Size(125, 113);
             this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -210,8 +210,10 @@
             // 
             // login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AcceptButton = this.guna2Button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoSize = true;
             this.BackColor = System.Drawing.SystemColors.Window;
             this.ClientSize = new System.Drawing.Size(409, 596);
             this.Controls.Add(this.pictureBox5);
@@ -224,6 +226,7 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.pictureBox9);
             this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
