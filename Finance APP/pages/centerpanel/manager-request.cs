64dd7cc6b1950fa69527eb17e00472a5e9406672
Methum <methum.edu@gmail.com>
2024-05-03 01:47:33 -0400

    final requests done

---
+++ Finance APP/pages/centerpanel/manager-request.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Finance_APP.pages.centerpanel
+{
+    public partial class manager_request : UserControl
+    {
+        public manager_request()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label17_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
