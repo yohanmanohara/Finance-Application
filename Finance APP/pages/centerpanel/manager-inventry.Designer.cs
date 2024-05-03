5aa9cf105486d0538169a1118439874f37d71b36
Methum <methum.edu@gmail.com>
2024-05-03 00:36:10 -0400

    Manager inventry log done

--- Finance APP/pages/centerpanel/manager-inventry.Designer.cs
+++ Finance APP/pages/centerpanel/manager-inventry.Designer.cs
@@ -74,7 +74,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI Variable Display", 27F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(375, 0);
+            this.label1.Location = new System.Drawing.Point(366, 0);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(295, 60);
