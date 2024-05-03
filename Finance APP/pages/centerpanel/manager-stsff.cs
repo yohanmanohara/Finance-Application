14883ea9a8bb72517b4e92beaa3e9aa851e08a26
Methum <methum.edu@gmail.com>
2024-05-02 23:30:19 -0400

    Done

---
+++ Finance APP/pages/centerpanel/manager-stsff.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Finance_APP.pages.centerpanel
+{
+    public partial class manager_stsff : UserControl
+    {
+        public manager_stsff()
+        {
+            InitializeComponent();
+        }
+
+        private void guna2TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
